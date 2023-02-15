ba722afe86ef973496029c9bb3c1bfb330ce734b
eumouz <125469962+eumouz@users.noreply.github.com>
2023-02-15 10:39:45 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//declarar variavel com let
+let numero = 1;
+
+{
+  let numero = 2;
+  console.log("dentro = ", numero);
+}
+
+console.log("fora =", numero);
+
+//declarando variável com var
+var numero1 = 1;
+
+{
+  var numero1 = 2;
+  console.log("dentro1=", numero1);
+}
+console.log("fora1=", numero1);
+
+//declarando uma constante
+
+//const numero2 = 10
+//numero2 = 11
+
+//console.log(numero2)
